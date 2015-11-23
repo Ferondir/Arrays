b91f7aefc7065fdcb7111c2e2841d75a556d67f8
Pavel Fedoseev <fedoseevpavel@gmail.com>
2015-11-23 21:30:33 +0600

    first commit

---
+++ src/com/Arrays/ArrayDemo.java
@@ -0,0 +1,9 @@
+package com.Arrays;
+
+class ArrayDemo{
+	public static void main(String[] args){
+		System.out.println("Hello Git!");
+		System.out.println("");
+		System.out.println("");
+	}
+}
---
+++ start.cmd
@@ -0,0 +1,5 @@
+javac -sourcepath ./src -d ./bin src/com/Arrays/ArrayDemo.java
+pause
+
+java -classpath ./bin com.Arrays.ArrayDemo
+pause
