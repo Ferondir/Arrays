155926de321f2048ae29f408277c48ae34e4704e
Pavel Fedoseev <fedoseevpavel@gmail.com>
2015-11-23 22:12:05 +0600

    NEW BRACH 1

--- src/com/Arrays/ArrayDemo.java
+++ src/com/Arrays/ArrayDemo.java
@@ -4,5 +4,10 @@ class ArrayDemo{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		System.out.println("N O T H I N G  H E R E");
+		System.out.println("NEW BRACH 1");
+		System.out.println("NEW BRACH 1");
+		System.out.println("NEW BRACH 1");
+		System.out.println("NEW BRACH 1");
+		
 	}
 }
