41a2d43723e9f22902411ac65a5b8a1eec9ff415
Pavel Fedoseev <fedoseevpavel@gmail.com>
2015-11-23 22:09:07 +0600

    new_branch o la la

--- src/com/Arrays/ArrayDemo.java
+++ src/com/Arrays/ArrayDemo.java
@@ -4,5 +4,7 @@ class ArrayDemo{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		System.out.println("N O T H I N G  H E R E");
+		
+		System.out.println("O la la");
 	}
 }
