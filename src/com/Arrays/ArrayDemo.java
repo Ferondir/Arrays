04c5bf2340cfdd406feed4ca867853c1e552700f
Pavel Fedoseev <fedoseevpavel@gmail.com>
2015-11-23 21:59:02 +0600

    new_branch nothinghere

--- src/com/Arrays/ArrayDemo.java
+++ src/com/Arrays/ArrayDemo.java
@@ -3,6 +3,6 @@ package com.Arrays;
 class ArrayDemo{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
-		System.out.println("Bye bye Git!");
+		System.out.println("N O T H I N G  H E R E");
 	}
 }
