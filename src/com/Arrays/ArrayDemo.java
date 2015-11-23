a22281032af1d65da37fa61c132d81009bd30112
Pavel Fedoseev <fedoseevpavel@gmail.com>
2015-11-23 21:36:55 +0600

    second commit

--- src/com/Arrays/ArrayDemo.java
+++ src/com/Arrays/ArrayDemo.java
@@ -4,6 +4,6 @@ class ArrayDemo{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		System.out.println("");
-		System.out.println("");
+		System.out.println("Bye bye Git!");
 	}
 }
